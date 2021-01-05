51465066cb2e2c13a719953775d3178bb1c1817b
ahmadraza100 <ahmadrazashafi@gmail.com>
2021-01-05 22:23:49 +0500

    Social Media Time Waster Count Ready

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 
-function f1(){
-    let f =document.getElementById("fb").value;
+
+
+                          
+                          // SM-CALCULATOR
+
+function f1() {
+    let f = document.getElementById("fb").value;
     let facebook = parseInt(f);
-    let i =document.getElementById("insta").value;
+    let i = document.getElementById("insta").value;
     let instagram = parseInt(i);
-    let y =document.getElementById("youtube").value;
+    let y = document.getElementById("youtube").value;
     let youtube = parseInt(y);
-    let s =document.getElementById("Snapchat").value;
-    let snapchat = parseInt(s)
+    let s = document.getElementById("Snapchat").value;
+    let snapchat = parseInt(s);
 
-    let t =document.getElementById("year").value;
-    let time = parseInt(t)
-    let total = (facebook+youtube+instagram+snapchat);
-    let totaltodays = total*365;
+    let t = document.getElementById("year").value;
+    let time = parseInt(t);
+    let total = (facebook + youtube + instagram + snapchat);
+    let totaltodays = total * 365;
 
 
-    ytom(time,totaltodays);
+    ytom(time, totaltodays);
 
-    function ytom(t,totall){
-        let yearTime =2021 -t ;
-        let totalT = totall*yearTime;
-        
-        
-        
-    }
+
+}
+
+function ytom(t, totall) {
+
+
+    let yearTime = 2021 - t;
+    let totalT = Math.floor((totall * yearTime)/24);
     
-    let a =  document.getElementsByClassName("resultt").style.display= "hidden";
- 
+    
+        document.querySelector("#result").innerHTML = `<h1 syle="text-align:centre">You spent <br /> \"<b>${totalT}</b>"\ Days <br/>of your Life on Social Media</h1>`;
     
 }
+
+document.getElementById("btn").addEventListener("click", function () {
+    f1();
+
+    document.getElementById("btn").style.display = "none";
+    document.querySelector("#r").style.display = "none";
+
+
+    document.querySelector("#result").style.display = "block";
+    document.querySelector("#result").style.fontSize = "2rem";
+
+    document.querySelector("#form").style.visibility = "hidden";
+
+    document.querySelector("#result").innerHTML.style.padding = "40px";
+
+
+})
+
+
+
+
+                //   Age to Hours
